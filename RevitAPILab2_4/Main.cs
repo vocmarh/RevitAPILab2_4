a2708253bab57534bade83cd2a5bba0d0efdbb6d
Сергей Храмцов <vocmarh@mail.ru>
2022-02-18 09:23:52 +0500

    Добавьте файлы проекта.

---
+++ RevitAPILab2_4/Main.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILab2_4
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var ducts = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                .OfClass(typeof(Duct))
+                .Cast<Duct>()
+                .ToList();
+
+            TaskDialog.Show("Ducts info", ducts.Count.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
